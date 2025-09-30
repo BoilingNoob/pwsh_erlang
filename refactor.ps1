f12abc3c4af80b59443e7f23fa66bbf51f9ee96f
BoilingNoob <84647732+BoilingNoob@users.noreply.github.com>
2025-09-29 22:07:32 -0400

    -created func to calculate service level -created func to walk towards desired service level

--- refactor.ps1
+++ refactor.ps1
@@ -63,6 +63,47 @@ function calculate_wait_probability() {
     [double]$pw = $x / ($x + $y)  
     return $pw 
 }
+function calulate_service_level() {
+    param(
+        $number_of_calls = 25,
+        $average_Handle_time = 15,
+        $target_answer_time = 15,
+        $agents = 10
+    )
+    $volume = calc_intensity -number_of_calls $number_of_calls -average_Handle_time $average_Handle_time
+    [double]$pw = calculate_wait_probability -number_of_calls $number_of_calls -average_Handle_time $average_Handle_time -agents $agents -volume $volume
+    [double]$service_level = 1 - ($pw * ([math]::Pow([math]::E, (-1 * (($agents - $volume) * ($target_answer_time / $average_Handle_time))))))
+
+    $temp = [pscustomobject]@{
+        agents        = $agents
+        volume        = $volume
+        pw            = $pw
+        service_level = $service_level
+    }
+    return $temp
+}
+
+function walk_to_min_agents_for_sla() {
+    param(
+        $number_of_calls = 25,
+        $average_Handle_time = 15,
+        $target_answer_time = 15,
+        $min_service_level = 0.9,
+        $agent_starting_point = 1
+    )
+    $agent_count = $agent_starting_point - 1
+    $calc_service_level = 0
+    while ($calc_service_level -lt $min_service_level -or $calc_service_level -eq "NaN") {
+        $agent_count++
+        write-host "agents: $agent_count"
+        $temp = calulate_service_level -number_of_calls $number_of_calls -average_Handle_time $average_Handle_time -target_answer_time $target_answer_time -agents $agent_count
+        write-host "temp: $temp"
+        $calc_service_level = $temp.service_level
+        write-host "agents: $agent_count, service level: $calc_service_level"
+    }
+
+    return $agent_count
+}
 
 function test_gambit_of_agents() {
     param(
@@ -94,4 +135,6 @@ function test_gambit_of_agents() {
 }
 
 
-test_gambit_of_agents -min_agents 10 -max_agents 60 -number_of_calls 100 -average_Handle_time 15 -target_handle_time 15
+#test_gambit_of_agents -min_agents 10 -max_agents 60 -number_of_calls 100 -average_Handle_time 15 -target_handle_time 15
+
+walk_to_min_agents_for_sla -number_of_calls 45 -average_Handle_time 15 -target_answer_time 0.3 -min_service_level 0.90 -agent_starting_point 1
